29b539b79084e1db8ef3e1afc77231506be0b6a7
Henrique Custódia <martinshenrique8@gmail.com>
2022-10-11 17:09:19 -0300

    Add OnPush Strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
-import { classManager } from './functions/class-manager';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
+import { createClassManager } from './functions/class-manager';
 import { getHost } from './functions/get-host';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
 
+  changeDetectorRef = inject(ChangeDetectorRef);
+
   host = getHost<HTMLElement>();
 
-  classManagerInstance = classManager(getHost(), 'red-color');
+  classManager = createClassManager(getHost(), 'red-color');
 
   setFontColorAsRed() {
-    this.classManagerInstance.add();
+    this.classManager.add();
   }
 
   removeFontColor() {
-    this.classManagerInstance.remove();
+    this.classManager.remove();
   }
 
 }
--- src/app/functions/class-manager.ts
+++ src/app/functions/class-manager.ts
@@ -1,6 +1,6 @@
 import { inject, Renderer2 } from "@angular/core";
 
-export function classManager(el: HTMLElement, className: string) {
+export function createClassManager(el: HTMLElement, className: string) {
     const renderer = inject(Renderer2);
 
     return {
